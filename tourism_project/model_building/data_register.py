bb5270f2967e3c4715fbf50bf7c620f9813c5491
enobongjimie-ux <enobongjimie@gmail.com>
2026-09-12 18:03:30 +0100

    fix: RAW_PATH to data/raw/

--- tourism_project/model_building/data_register.py
+++ tourism_project/model_building/data_register.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-RAW_PATH = "tourism_project/data/tourism.csv"  # path to the raw tourism.csv file inside the data folder
+RAW_PATH = "tourism_project/data/raw/tourism.csv"  # path to the raw tourism.csv file inside the data folder
 
 # Load the raw dataset
 df = pd.read_csv(RAW_PATH)
